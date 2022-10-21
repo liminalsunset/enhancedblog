2b48118d2c22b15c75a78a60157b99f720f22be4
dev-yoga <spin_shoppe@icloud.com>
2022-10-20 21:30:10 -0700

    tidying

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name= 'blog'
 urlpatterns = [
-    path('', views.post_list, name='post_list')
-    path('<int:year>/<int:month>/<int:day>/<slug_post>/', views.post_detail, name='post_detail'),
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'),
 ]
