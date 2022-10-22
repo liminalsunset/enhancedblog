f0425e26ff160ac3be9455e953d4086eadf53c7e
dev-yoga <spin_shoppe@icloud.com>
2022-10-21 19:18:38 -0700

    post list view

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from calendar import month
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -22,3 +23,9 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
