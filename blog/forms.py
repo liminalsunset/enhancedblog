8f8aa2671c18be4c0b1d31a9624d6b462eca4149
dev-yoga <spin_shoppe@icloud.com>
2022-10-21 21:18:28 -0700

    comment form

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 
 from django import forms
+from .models import Comment
 
 
 class EmailPostForm(forms.Form):
@@ -8,3 +9,9 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False,
                                widget=forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+        
