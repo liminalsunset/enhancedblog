8082cb87b8f13cd0cdbb440596b28a5dcd96be8a
dev-yoga <spin_shoppe@icloud.com>
2022-10-21 20:05:35 -0700

    email form for post share works, confirms sent, displays errors

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,10 @@
+
 from django import forms
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    to = forms.EmailField()
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea)
