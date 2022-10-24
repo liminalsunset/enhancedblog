18b2cdca9345fb3967ded653a3820e19271fd493
dev-yoga <spin_shoppe@icloud.com>
2022-10-23 21:31:38 -0700

    Tagging works, displays in admin, searchable by tag in admin

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -25,6 +26,7 @@ class Post(models.Model):
                               default='draft')
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
     class Meta:
         ordering = ('-publish',)
 
@@ -50,3 +52,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.name} on {self.post}'
+
